0be1ed6580260d188bec7d185ec63d01903b5dd3
lakshyasukhralia <lakshyasukhralia97@gmail.com>
2022-06-09 18:02:56 +0530

    Health route added

--- src/main/kotlin/com/sukhralia/Application.kt
+++ src/main/kotlin/com/sukhralia/Application.kt
@@ -3,6 +3,7 @@ package com.sukhralia
 import com.sukhralia.config.database.JobMongoDatabase
 import com.sukhralia.data.repository.mongo.JobMongoRepository
 import com.sukhralia.config.plugins.configureSerialization
+import com.sukhralia.rest.routes.setupHealthRoutes
 import com.sukhralia.rest.routes.setupJobRoutes
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -11,6 +12,7 @@ fun main() {
 
     embeddedServer(Netty, port = System.getenv("PORT")?.toInt() ?: 8072, host = "0.0.0.0") {
         configureSerialization()
+        setupHealthRoutes()
         setupJobRoutes(JobMongoRepository(JobMongoDatabase().provideJobCollection()))
     }.start(wait = true)
 
---
+++ src/main/kotlin/com/sukhralia/rest/routes/HealthRoutes.kt
@@ -0,0 +1,17 @@
+package com.sukhralia.rest.routes
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.setupHealthRoutes() {
+
+    routing {
+
+            get("/") {
+                call.respondText { "Healthy" }
+            }
+
+    }
+
+}
